52f8a83958d67159bf4326fa2841bdc5866bd11d
Anders Bech Mellson <mellson@icloud.com>
2015-01-16 19:57:49 +0100

    Added playground file

---
+++ Swift-Utils.playground/section-1.swift
@@ -0,0 +1,28 @@
+// Anders Bech Mellson, Utility Playground
+
+import Cocoa
+
+func time <A> (f: @autoclosure () -> A) -> (result: A, duration: String) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    let result = f()
+    let endTime = CFAbsoluteTimeGetCurrent()
+    return (result, "Elapsed time is \(endTime - startTime) seconds.")
+}
+
+time (1+2)
+
+func isPrime(var number:Int) ->Bool {
+    var i = 0;
+    for i=2; i<number; i++ {
+        if(number % i == 0 && i != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let (res, dur) = time(isPrime(Int.max))
+res
+dur
+
+
